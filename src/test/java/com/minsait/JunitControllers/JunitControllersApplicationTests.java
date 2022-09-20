f8eec945ed19549b0472538e1edbdd3a1cb762da
Elias Muñoz <emunozp@minsait.com>
2022-09-20 18:05:11 -0500

    Almost finishing tests

--- src/test/java/com/minsait/JunitControllers/JunitControllersApplicationTests.java
+++ src/test/java/com/minsait/JunitControllers/JunitControllersApplicationTests.java
@@ -8,7 +8,6 @@ import com.minsait.JunitControllers.repositories.CuentaRepository;
 import com.minsait.JunitControllers.services.CuentaServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestReporter;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -24,76 +23,87 @@ import static org.mockito.Mockito.*;
 @ExtendWith(MockitoExtension.class)
 class JunitControllersApplicationTests {
 
-	Cuenta cuenta;
-	Banco banco;
+    Cuenta cuenta;
+    Banco banco;
 
-	@BeforeEach
-	void setUp(){
-		this.cuenta = new Cuenta();
-		this.cuenta.setId(10L);
-		this.cuenta.setPersona("PersonaTest");
-		this.cuenta.setSaldo(new BigDecimal(10));
-		this.banco = new Banco();
-		this.banco.setId(10L);
-		this.banco.setNombre("Banco");
-		this.banco.setNombre(this.banco.getNombre()+"test");
-		this.banco.setTotalTransferencias(0);
-	}
-	@Mock
-	BancoRepository bancoRepository;
+    @BeforeEach
+    void setUp() {
+        this.cuenta = new Cuenta();
+        this.cuenta.setId(10L);
+        this.cuenta.setPersona("PersonaTest");
+        this.cuenta.setSaldo(new BigDecimal(10));
+        this.banco = new Banco();
+        this.banco.setId(10L);
+        this.banco.setNombre("Banco");
+        this.banco.setNombre(this.banco.getNombre() + "test");
+        this.banco.setTotalTransferencias(0);
+    }
 
-	@Mock
-	CuentaRepository cuentaRepository;
+    @Mock
+    BancoRepository bancoRepository;
 
-	@InjectMocks
-	CuentaServiceImpl service;
+    @Mock
+    CuentaRepository cuentaRepository;
+
+    @InjectMocks
+    CuentaServiceImpl service;
 
 
-	@Test
-	void testFindCuentaById() {
-		when(cuentaRepository.findById(anyLong())).thenReturn(Datos.CUENTAS.get(0));
-		assertEquals("Ana",service.findById(1L).getPersona());
-	}
+    @Test
+    void testFindCuentaById() {
+        when(cuentaRepository.findById(anyLong())).thenReturn(Datos.CUENTAS.get(0));
+        assertEquals("Ana", service.findById(1L).getPersona());
+    }
 
-	@Test
-	void testRevisarTotalTrans(){
-		when(bancoRepository.findById(anyLong())).thenReturn(Datos.BANCO);
-		assertEquals(Datos.BANCO.getTotalTransferencias(),service.revisarTotalTransferencias(4L));
-	}
+    @Test
+    void testRevisarTotalTrans() {
+        when(bancoRepository.findById(anyLong())).thenReturn(Datos.BANCO);
+        assertEquals(Datos.BANCO.getTotalTransferencias(), service.revisarTotalTransferencias(4L));
+    }
 
-	@Test
-	void testRevisarSaldo(){
-		when(cuentaRepository.findById(anyLong())).thenReturn(Datos.CUENTA);
-		assertEquals(Datos.CUENTA.getSaldo(),service.revisarSaldo(4L));
-	}
+    @Test
+    void testRevisarSaldo() {
+        when(cuentaRepository.findById(anyLong())).thenReturn(Datos.CUENTA);
+        assertEquals(Datos.CUENTA.getSaldo(), service.revisarSaldo(4L));
+    }
 
-	@Test
-	void testTransferir(){
-		when(cuentaRepository.findById(anyLong())).thenReturn(Datos.CUENTAS.get(0));
-		when(cuentaRepository.findById(anyLong())).thenReturn(Datos.CUENTAS.get(1));
-		when(bancoRepository.findById(anyLong())).thenReturn(Datos.BANCOS.get(0));
-		service.transferir(1L,2L,new BigDecimal(100),1L);
-		assertEquals(Datos.CUENTAS.get(0).getSaldo(),new BigDecimal("1500.50"));
-	}
+    @Test
+    void testTransferir() {
+        when(cuentaRepository.findById(anyLong())).thenReturn(Datos.CUENTAS.get(0));
+        when(cuentaRepository.findById(anyLong())).thenReturn(Datos.CUENTAS.get(1));
+        when(bancoRepository.findById(anyLong())).thenReturn(Datos.BANCOS.get(0));
+        service.transferir(1L, 2L, new BigDecimal(100), 1L);
+        assertEquals(Datos.CUENTAS.get(0).getSaldo(), new BigDecimal("1500.50"));
+    }
 
-	@Test
-	void testDoThrowTransferir(){
-		doThrow(DineroInsuficienteException.class).when(cuentaRepository).findById(anyLong());
-		Cuenta cuentaOrigen = Datos.CUENTA;
-		cuentaOrigen.setId(5L);
-		cuentaOrigen.setPersona("Elias Muñoz");
-		cuentaOrigen.setSaldo(new BigDecimal("0"));
-		assertThrows(
-				DineroInsuficienteException.class,
-				() -> service.transferir(
-						cuentaOrigen.getId(),
-						Datos.CUENTAS.get(0).getId(),
-						new BigDecimal(100),
-						Datos.BANCOS.get(0).getId()
-				)
+    @Test
+    void testDoThrowTransferir() {
+        doThrow(DineroInsuficienteException.class).when(cuentaRepository).findById(anyLong());
+        Cuenta cuentaOrigen = Datos.CUENTA;
+        cuentaOrigen.setId(5L);
+        cuentaOrigen.setPersona("Elias Muñoz");
+        cuentaOrigen.setSaldo(new BigDecimal("0"));
+        assertThrows(
+                DineroInsuficienteException.class,
+                () -> service.transferir(
+                        cuentaOrigen.getId(),
+                        Datos.CUENTAS.get(0).getId(),
+                        new BigDecimal(100),
+                        Datos.BANCOS.get(0).getId()
+                )
+        );
+    }
+
+    @Test
+    void testExceptionRetirar() {
+        Exception e = assertThrows(DineroInsuficienteException.class, () -> {
+            cuenta.retirar(new BigDecimal(100));
+        });
+		assertEquals(
+				"Dinero Insuficiente en la cuenta.",
+				e.getMessage()
 		);
-	}
-
+    }
 
 
 }
